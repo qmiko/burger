3f3d831d17edd3e621ad267f706ccc222d16a00c
shaquima <senoritabroccolini@gmail.com>
2019-08-13 21:14:11 -0500

    fixing port...

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
@@ -26,4 +26,7 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(PORT, function(){
+    console.log("App listening on PORT:" + PORT);
+
+});
